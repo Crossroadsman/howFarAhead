4a274920fd3405179df21072c7fd451f44ae54bb
Crossroadsman <crossroadsman@gmail.com>
2016-08-11 23:43:32 -0230

    Add minimum functionality for distance mode
    Still need to do some clean up (make keyboard automatically
    disappear, pretty format the output, make text input overwrite
    existing zeros, etc)
    
    Also need to do implementation for time mode.
    
    Then need to do various UI prettification tweaks

--- How Far Ahead/How Far Ahead/DistanceFromTimeViewController.swift
+++ How Far Ahead/How Far Ahead/DistanceFromTimeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DistanceFromTimeViewController: UIViewController {
     
+    // TODO: Need to set cursor to highlight (overwrite) existing value when textfield selected
+    
+    // TODO: Need to make keyboard disappear automatically.
     
     @IBOutlet weak var fasterTextField: UITextField!
     
@@ -22,15 +25,31 @@ class DistanceFromTimeViewController: UIViewController {
     
     @IBAction func computeButtonWasPressed(_ sender: UIButton) {
         
-        guard Double(fasterTextField.text!) > Double(slowerTextField.text!) else { return }
-        
-        // TODO: code to calculate distance goes here
+        calculateDistance()
         
     }
     
-    func timeFromTimeSincePassedTextField() {
+    
+    
+    func calculateDistance() {
+        // e.g. faster is travelling at 60 kmh
+        //      slower is travelling at 30 kmh
+        //      faster passed slower 5 minutes ago
+        //      how far ahead is faster now
+        //      (bonus, when will slower reach faster's current location?)
+        let fasterSpeed = Double(fasterTextField.text ?? "0")!
+        let slowerSpeed = Double(slowerTextField.text ?? "0")!
+        let netSpeed = fasterSpeed - slowerSpeed
         
-        // TODO: take a mm:ss formatted value from the text field and
+        let minutesSincePassed = Double(timeSincePassedTextField.text ?? "0")! // minutes
+        let hoursSincePassed = minutesSincePassed / 60
+        
+        let distance = netSpeed * hoursSincePassed
+        
+        // TODO: tidy the string formatting of the output (round to 2DP?)
+        distanceTextField.text = String(distance)
+        
+        
     }
 
     override func viewDidLoad() {
