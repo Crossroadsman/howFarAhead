162df7200d2386357e35a82081ef0f361806cf0f
Crossroadsman <crossroadsman@gmail.com>
2016-08-19 18:38:29 -0300

    Start adding time functionality

--- How Far Ahead/How Far Ahead/TimeFromDistanceViewController.swift
+++ How Far Ahead/How Far Ahead/TimeFromDistanceViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class TimeFromDistanceViewController: UIViewController {
 
+    @IBOutlet weak var fasterTextField: UITextField!
+    
+    @IBOutlet weak var slowerTextField: UITextField!
+    
+    @IBOutlet weak var timeSincePassedTextField: UITextField!
+    
+    @IBOutlet weak var timeToReachTextField: UITextField!
+    
+    
+    @IBAction func computeButtonWasPressed(_ sender: AnyObject) {
+        calculateTime()
+    }
+    
+    func calculateDistance() {
+        // e.g. faster is travelling at 60 kmh
+        //      slower is travelling at 30 kmh
+        //      faster passed slower 5 minutes ago
+        //      how far ahead is faster now
+        //      (bonus, when will slower reach faster's current location?)
+        let fasterSpeed = Double(fasterTextField.text ?? "0")!
+        let slowerSpeed = Double(slowerTextField.text ?? "0")!
+        let netSpeed = fasterSpeed - slowerSpeed
+        
+        let minutesSincePassed = Double(timeSincePassedTextField.text ?? "0")! // minutes
+        let hoursSincePassed = minutesSincePassed / 60
+        
+        let distance = netSpeed * hoursSincePassed
+        let roundedDistance = round(distance * 100) / 100
+        
+        // done: tidy the string formatting of the output (round to 2DP?)
+        distanceTextField.text = String(roundedDistance)
+        
+        
+    }
+    
+    func calculateTime() {
+        
+        // use result of calculateDistance() to determine time to reach
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
